06cf090820159546e5e10a89a6bd342ebc7035b7
HalonixTheFirst <mfawadshah2@gmail.com>
2026-04-25 21:56:44 +0500

    Problem-RottingOranges.cpp using bfs

--- Problem-ConnectedComponents.cpp
+++ Problem-ConnectedComponents.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 class Solution {
 public:
-  int countComponents(int V, vector<vector<int> > &edges) {
+  int countComponents(int V, vector<vector<int>> &edges) {
     //The edges matrix only has edges from a to b ;
     // edge between 0 and 1, 1 and 2; etc;
-    vector<vector<int> > adj(V);
+    vector<vector<int>> adj(V);
     for (auto pair: edges) {
       adj[pair[0]].push_back(pair[1]);
       adj[pair[1]].push_back(pair[0]);
