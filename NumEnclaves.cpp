a7b891e83fb79e64c4ea79c6d865df8b02494911
HalonixTheFirst <mfawadshah2@gmail.com>
2026-05-04 23:28:36 +0500

    Problem-NumEnclaves.cpp using dfs

--- NumEnclaves.cpp
+++ NumEnclaves.cpp
@@ -4,6 +4,18 @@ class Solution {
   private:
     int n,m;
 public:
+    void dfs(vector<vector<int>>& grid, vector<vector<int>>& vis,int i ,int j) {
+      vis[i][j]=true;
+      int drow[]= {0,-1,0,1};
+      int dcol[]= {-1,0,1,0};
+      for (int k=0;k<4;k++) {
+        int nrow=i+drow[k];
+        int ncol=j+dcol[k];
+        if (ncol>=0 && ncol<m && nrow>=0 && nrow<m && !vis[nrow][ncol] && grid[nrow][ncol]) {
+          dfs(grid,vis,nrow,ncol);
+        }
+      }
+    }
     int numEnclaves(vector<vector<int>>& grid) {
       n=grid.size();
       m=grid[0].size();
@@ -12,7 +24,14 @@ public:
       for(int i =0; i<n;i++){
         for(int j =0; j<m ;j++){
           if (i==0 || i == n-1 || j ==0 || j == m-1 ) {
-            if (grid[i][j]==1)
+            if (grid[i][j]==1) dfs(grid,vis,i,j);
+          }
+        }
+      }
+      for(int i =0; i<n;i++){
+        for(int j =0; j<m ;j++){
+          if (grid[i][j]==1 && !vis[i][j]) {
+            count++;
           }
         }
       }
