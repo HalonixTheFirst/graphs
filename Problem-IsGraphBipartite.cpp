090a4717113848add314f26d318d8af8bebac10c
HalonixTheFirst <mfawadshah2@gmail.com>
2026-05-09 13:25:36 +0500

    Problem-IsGraphBipartite.cpp using dfs

--- Problem-IsGraphBipartite.cpp
+++ Problem-IsGraphBipartite.cpp
@@ -1,15 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 class Solution {
 private:
   bool dfs(int v, bool c,vector<int> &vis,vector<vector<int>>& graph) {
-    vis[v]= c ? 1:2;
+    if(c) vis[v]=1;
+    else vis[v]=2;
     for (auto i : graph[v]) {
       if (vis[i]) {
         if (vis[i]==vis[v]) return false;
       }
       else {
-        return dfs(i,!c,vis,graph);
+        if (dfs(i,!c,vis,graph)==false) return false;;
       }
     }
     return true;
@@ -19,7 +22,7 @@ public:
     vector<int> vis(graph.size(),0);
     for(int i = 0; i < graph.size();i++) {
       if(!vis[i]){
-        if(dfs(i,1,vis,graph)==false) return false;
+        if(!dfs(i,1,vis,graph)) return false;
       }
     }
     return true;
