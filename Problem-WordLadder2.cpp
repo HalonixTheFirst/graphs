8d61edd8eda993ab111bec9bf26e0aed35668a16
HalonixTheFirst <mfawadshah2@gmail.com>
2026-05-13 16:21:24 +0500

    WordLadder2.cpp using dfs

--- Problem-WordLadder2.cpp
+++ Problem-WordLadder2.cpp
@@ -5,35 +5,22 @@
 using namespace std;
 class Solution {
 private:
-  string nextWord(vector<string> &list,unordered_set<string> &s,queue<vector<string>> &q) {
-    string word = list.back();
-    string change;
-    for (int i=0 ;i<word.size();i++){
-      string x = word;
-      for (char ch = 'a' ; ch<='z' ; ch++) {
-        x[i] = ch;
-        if (s.count(x)) {
-          auto new_list = list;
-          new_list.push_back(x);
-          q.push(new_list);
-          change = x;
-        }
-      }
-    }
-    return change;
-  }
 public:
   vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
     queue<vector<string>> q;
     q.push({beginWord});
     vector<vector<string>> ans;
+    vector<string> used;
     unordered_set<string> s(wordList.begin(),wordList.end());
     while (q.size()) {
-      auto list = q.front();
-      q.pop();
-      string word = nextWord(list,s,q);
-      s.erase(word);
-      if (list.back()==endWord) ans.push_back(list);
+      if (!ans.empty()) return ans;
+      for (auto w : used) s.erase(w);
+      used.clear();
+      for (int i =0; i<q.size();i++) {
+        auto list = q.front();
+        q.pop();
+        nextWord(list,s,q,used);
+      }
     }
     return ans;
   }
