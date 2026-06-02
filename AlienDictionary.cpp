086f527e390124c2ce8a02024df1c6c8d8497472
HalonixTheFirst <mfawadshah2@gmail.com>
2026-06-02 23:39:23 +0500

    update AlienDictionary.cpp

--- AlienDictionary.cpp
+++ AlienDictionary.cpp
@@ -1,6 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
+  //Compare two words . like 1st and 2nd.
+  // find the first letter that isnt equal;
+  // Create a directed edge between the first letter and the second letter
+  // run a topo sort
+  // return the topo sort
 public:
   string findOrder(vector<string> &words) {
 
